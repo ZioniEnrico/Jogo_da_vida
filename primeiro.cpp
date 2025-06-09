dd98acf51b6b0d1746f6e8162031b77d0574e989
ZioniEnrico <enrico14zioni@hotmail.com>
2025-06-09 20:34:58 -0300

    Primeiro arquivo cpp adicionado

---
+++ primeiro.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h> // para getch
+#include<stdlib.h>
+#include<ctype.h> // para tolower e toupper
+#include<string.h> // para strlwr e strupr
+#include<locale.h>
+
+int main () {
+    setlocale(LC_ALL, "portuguese");
+    printf("teste");}
